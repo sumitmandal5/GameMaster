2111523a819c77fd999e38b6ac926a5456f5ee68
sumitmandal5 <sumit.kumar03@gmail.com>
2025-03-01 16:12:46 +0000

    Resolution of CORS issue

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 # Initializes the Flask app.
 
 from flask import Flask
+from flask_cors import CORS
 from app.routes import router
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, origins=["http://localhost:4200"])
     app.register_blueprint(router)  # Register routes
     return app
